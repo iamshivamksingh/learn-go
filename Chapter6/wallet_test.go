b4287cfbf29097ae93769c36929820117aa16b11
sksingh <sksingh@gmail.com>
2022-06-12 22:04:36 +0530

    Added few comments

--- Chapter6/wallet_test.go
+++ Chapter6/wallet_test.go
@@ -5,32 +5,37 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
 
-		wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
 		got := wallet.Balance()
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		// Creating an empty wallet
+		wallet := Wallet{}
+
+		// Depositing money into the wallet
+		wallet.Deposit(Bitcoin(10))
+
+		// Asserting the balance
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
+		// Creating a wallet with 20 Bitcoins
 		wallet := Wallet{balance: Bitcoin(20)}
 
+		// Withdrawing the money from the wallet
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		// Asserting the balance
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
