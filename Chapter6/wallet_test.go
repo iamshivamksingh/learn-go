a9063be813401b2c5447b862685ab7cae20d2ac5
sksingh <sksingh@gmail.com>
2022-06-14 21:02:45 +0530

    Added test cases to validate both sufficient & insufficient funds

--- Chapter6/wallet_test.go
+++ Chapter6/wallet_test.go
@@ -17,7 +17,17 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		// Creating a wallet with 20 Bitcoins
 		wallet := Wallet{startingBalance}
@@ -41,6 +51,13 @@ func assertError(t testing.TB, got, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
