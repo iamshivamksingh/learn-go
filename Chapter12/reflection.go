e378c4e4e3b2b4cea957346c79c8d45c8674a909
sksingh <sksingh@gmail.com>
2022-06-19 01:27:39 +0530

    added test cases to validate slice, array, map & func

--- Chapter12/reflection.go
+++ Chapter12/reflection.go
@@ -9,14 +9,62 @@ import "reflect"
 func Walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+	// The number of fields
+	numberOfValues := 0
+	// Way to extract the value (Field or Index)
+	var getField func(int) reflect.Value
 
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.String:
+		fn(val.String())
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			Walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		Walk(getField(i).Interface(), fn)
+	}
+}
+
+func WalkNew(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		// iterating through the channel to receive
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
 		}
 	}
 }
@@ -24,6 +72,7 @@ func Walk(x interface{}, fn func(input string)) {
 func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
 
+	// if x is a pointer to struct
 	if val.Kind() == reflect.Ptr {
 		// extracing the underlying value
 		val = val.Elem()
--- Chapter12/reflection_test.go
+++ Chapter12/reflection_test.go
@@ -60,6 +60,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"slices",
+			[]Profile{
+				{22, "London"},
+				{33, "New York"},
+			},
+			[]string{"London", "New York"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{22, "London"},
+				{33, "New York"},
+			},
+			[]string{"London", "New York"},
+		},
 	}
 
 	for _, test := range cases {
@@ -74,5 +90,73 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		m := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		WalkNew(m, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		profileChannel := make(chan Profile)
+
+		go func() {
+			profileChannel <- Profile{33, "Berlin"}
+			profileChannel <- Profile{34, "Katowice"}
+			close(profileChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		WalkNew(profileChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		WalkNew(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t testing.TB, result []string, word string) {
+	t.Helper()
+
+	exists := false
+	for _, val := range result {
+		if word == val {
+			exists = true
+		}
+	}
+
+	if !exists {
+		t.Errorf("expected %+v to contain %q but it didn't", result, word)
 	}
 }
