ae3e92f0c770d27f42eabd62328b5f92d74d28de
sksingh <sksingh@gmail.com>
2022-06-19 21:55:17 +0530

    learning context

---
+++ Chapter14/context_test.go
@@ -0,0 +1,33 @@
+package context1
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func (s *StubStore) Cancel() {
+
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	svr := Server(&StubStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("got %v, want %v", response.Body.String(), data)
+	}
+}
