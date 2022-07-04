5aca4587e2ced2d27871515da7687a800bb6258c
sksingh <sksingh@gmail.com>
2022-07-05 00:08:14 +0530

    arabic to roman conversion

---
+++ Chapter15/numeral.go
@@ -0,0 +1,29 @@
+package numeral
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func convertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
