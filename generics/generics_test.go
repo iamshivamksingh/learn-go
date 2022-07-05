30fc3d6fa87cdf0bdcc8a7b8cdc61488919c7ec2
Shivam Singh <sksingh@tekion.com>
2022-07-05 17:37:50 +0530

    added generics function

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -27,3 +27,33 @@ func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+
+	return result
+}
+
+func Sum(numbers []int) int {
+	add := func(acc, x int) int {
+		return acc + x
+	}
+	return Reduce(numbers, add, 0)
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+
+	return Reduce(numbers, sumTail, []int{})
+}
